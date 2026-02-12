2db63a5020a9cf5fd1f601a686459f3cf98931b4
Alwin18 <alwinbezarius@gmail.com>
2026-02-12 10:53:04 +0700

    added error log

--- internal/module/role/service.go
+++ internal/module/role/service.go
@@ -33,6 +33,7 @@ func (s *Service) ListRole(ctx *fiber.Ctx, params ListRoleRequest) ([]ListRoleRe
 	}
 
 	if err := queryResult.Count(&count).Error; err != nil {
+		s.logger.Error("failed to count roles", err)
 		return nil, 0, err
 	}
 
@@ -41,6 +42,7 @@ func (s *Service) ListRole(ctx *fiber.Ctx, params ListRoleRequest) ([]ListRoleRe
 		Offset((params.Page - 1) * params.PerPage).
 		Order("updated_at desc").
 		Find(&roles).Error; err != nil {
+		s.logger.Error("failed to find roles", err)
 		return nil, 0, err
 	}
 
@@ -61,26 +63,38 @@ func (s *Service) ListRole(ctx *fiber.Ctx, params ListRoleRequest) ([]ListRoleRe
 
 func (s *Service) CreateRole(ctx *fiber.Ctx, body CreateRoleRequest) error {
 	s.logger.Info("Create Role")
-	return s.db.Gorm.WithContext(ctx.UserContext()).Create(&models.Role{
+	if err := s.db.Gorm.WithContext(ctx.UserContext()).Create(&models.Role{
 		Code:        body.Code,
 		Name:        body.Name,
 		Description: body.Description,
 		CreatedAt:   time.Now(),
 		UpdatedAt:   time.Now(),
-	}).Error
+	}).Error; err != nil {
+		s.logger.Error("failed to create role", err)
+		return err
+	}
+	return nil
 }
 
 func (s *Service) UpdateRole(ctx *fiber.Ctx, id uint, body CreateRoleRequest) error {
 	s.logger.Info("Update Role")
-	return s.db.Gorm.WithContext(ctx.UserContext()).Model(&models.Role{}).Where("id = ?", id).Updates(&models.Role{
+	if err := s.db.Gorm.WithContext(ctx.UserContext()).Model(&models.Role{}).Where("id = ?", id).Updates(&models.Role{
 		Code:        body.Code,
 		Name:        body.Name,
 		Description: body.Description,
 		UpdatedAt:   time.Now(),
-	}).Error
+	}).Error; err != nil {
+		s.logger.Error("failed to update role", err)
+		return err
+	}
+	return nil
 }
 
 func (s *Service) DeleteRole(ctx *fiber.Ctx, id uint) error {
 	s.logger.Info("Delete Role")
-	return s.db.Gorm.WithContext(ctx.UserContext()).Delete(&models.Role{}, id).Error
+	if err := s.db.Gorm.WithContext(ctx.UserContext()).Delete(&models.Role{}, id).Error; err != nil {
+		s.logger.Error("failed to delete role", err)
+		return err
+	}
+	return nil
 }
